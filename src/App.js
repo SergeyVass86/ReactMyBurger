c4f9e02a61391f90b261d921bde30a7c1bfe22b5
SerVass <test@test.com>
2019-08-14 13:21:21 +0300

    First and final commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
+
+const asycnCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asycnOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+
+const asycnAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
+
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
+  render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={asycnAuth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={asycnCheckout} />
+          <Route path="/orders" component={asycnOrders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={asycnAuth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
+    return (
+      <div>
+        <Layout>{routes}</Layout>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
